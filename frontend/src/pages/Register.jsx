78dd7566ad6e6d75f861c04d22ba86a7e1f45207
krris <krrishkumar.0810@gmail.com>
2026-04-23 16:09:00 +0530

    register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,21 +2,43 @@ import { useState } from "react";
 import API from "../api/axios";
 
 export default function Register() {
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState({
+  name: "",
+  email: "",
+  password: ""
+});
 
   const submit = async () => {
+  console.log(form);  // 👈 add this
+
+  try {
     await API.post("/auth/register", form);
     alert("Registered successfully");
-  };
+  } catch (err) {
+    console.log(err.response.data);  // 👈 IMPORTANT
+    alert("Error");
+  }
+};
 
   return (
     <div className="container">
       <h2>Register</h2>
 
-      <input placeholder="Name" onChange={e => setForm({...form, name:e.target.value})}/>
-      <input placeholder="Email" onChange={e => setForm({...form, email:e.target.value})}/>
-      <input type="password" placeholder="Password" onChange={e => setForm({...form, password:e.target.value})}/>
+      <input
+  placeholder="Name"
+  onChange={e => setForm({ ...form, name: e.target.value })}
+/>
 
+<input
+  placeholder="Email"
+  onChange={e => setForm({ ...form, email: e.target.value })}
+/>
+
+<input
+  type="password"
+  placeholder="Password"
+  onChange={e => setForm({ ...form, password: e.target.value })}
+/>
       <button onClick={submit}>Register</button>
     </div>
   );
